81da13618f5b9a6daaf7faedd227bdba69e706d5
gupiccolimartins <gpiccolimartins@gmail.com>
2020-01-16 15:45:03 -0300

    Criando a classe Mensagem e MensagemView

---
+++ client/js/app/views/mensagemView.js
@@ -0,0 +1,12 @@
+class MensagemView{
+    constructor(elemento){
+        this._elemento = elemento;
+    }
+    _template(modelo){
+        return modelo.texto ? `<p class = "alert alert-info">${modelo.texto}</p> ` : '<p></p>' ;
+    }
+
+    update(modelo){
+        this._elemento.innerHTML = this._template(modelo);
+    }
+}
