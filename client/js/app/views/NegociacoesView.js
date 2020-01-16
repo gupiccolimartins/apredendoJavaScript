add92f9f50adb60c2ba7e016df9d874561ee6b1c
gupiccolimartins <gpiccolimartins@gmail.com>
2020-01-16 10:40:45 -0300

    Totalizando o volume no template

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -30,11 +30,19 @@ class NegociacoesView{
                             <td>${n.volume}</td>
                         </tr>
                     `
-                    
+
                 ).join('')}
             </tbody>
                 
             <tfoot>
+                    <td colspan="3"></td>
+                    <td>${
+                        (function() {
+                            let total = 0;
+                            modelo.negociacoes.forEach(n => total+=n.volume);
+                            return total;
+                    })() 
+                    }</td>
             </tfoot>
         </table>
         
