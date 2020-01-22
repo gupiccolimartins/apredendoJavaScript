c24460aa75642119086005dd2ae67651f36a4763
gupiccolimartins <gpiccolimartins@gmail.com>
2020-01-22 14:54:01 -0300

    API Reflection

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,12 +1,14 @@
 class ListaNegociacoes{
 
-    constructor(armadilha){
+    constructor(contexto, armadilha){
         this._negociacoes = [];
         this._armadilha = armadilha;
+        this._contexto = contexto;
     }
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
+        //this._armadilha(this);
+        Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 
     get negociacoes(){
@@ -16,7 +18,8 @@ class ListaNegociacoes{
     esvazia(){
 
         this._negociacoes = [];
-        this._armadilha(this);
+        //this._armadilha(this);
+        Reflect.apply(this._armadilha, this._contexto, [this]);
 
     }
 }
